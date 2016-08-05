33f5be36fb09903f04744fb0f262b031b4c53bbe
Andrew Marshall <andrew@johnandrewmarshall.com>
2016-08-05 00:08:30 -0400

    Extract adapters to separate file

--- lib/capybara/accessible.rb
+++ lib/capybara/accessible.rb
@@ -1,4 +1,5 @@
 require 'capybara'
+require 'capybara/accessible/adapters'
 require 'capybara/accessible/auditor'
 require 'capybara/accessible/capybara_extensions'
 require 'capybara/accessible/driver_extensions'
@@ -7,66 +8,6 @@ require "capybara/accessible/railtie" if defined?(Rails)
 
 module Capybara
   module Accessible
-    class SeleniumDriverAdapter
-      def modal_dialog_present?(driver)
-        begin
-          driver.browser.switch_to.alert
-          true
-        rescue ::Selenium::WebDriver::Error::NoAlertOpenError, ::NoMethodError
-          false
-        end
-      end
-
-      def failures_script
-        "return axs.Audit.auditResults(results).getErrors();"
-      end
-
-      def create_report_script
-        "return axs.Audit.createReport(results);"
-      end
-
-      def run_javascript(driver, script)
-        driver.execute_script(script)
-      end
-    end
-
-    class WebkitDriverAdapter
-      def modal_dialog_present?(driver)
-        driver.alert_messages.any?
-      end
-
-      def failures_script
-        "axs.Audit.auditResults(results).getErrors();"
-      end
-
-      def create_report_script
-        "axs.Audit.createReport(results);"
-      end
-
-      def run_javascript(driver, script)
-        driver.evaluate_script(script)
-      end
-    end
-
-    class PoltergeistDriverAdapter
-      def modal_dialog_present?(driver)
-        false
-      end
-
-      def failures_script
-        "return axs.Audit.auditResults(results).getErrors()"
-      end
-
-      def create_report_script
-        "return axs.Audit.createReport(results)"
-      end
-
-      def run_javascript(driver, script)
-        # Have to wrap in an anonymous function because of https://github.com/jonleighton/poltergeist/issues/198
-        driver.evaluate_script %{ (function() {#{script}})() }
-      end
-    end
-
     class << self
       def skip_audit
         Capybara::Accessible::Auditor.disable
