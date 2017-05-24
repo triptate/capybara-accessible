b8db66bf33403a3d8fd593418ccc7543aac952a6
Andrew Marshall <amarshall@casecommons.org>
2017-05-24 15:59:43 -0400

    Change method of checking for modal to avoid 2 second delay
    Switching to the alert waits to see if one will appear, which is not
    really what we care about here: just if it's present right now. This
    changes to attempting to navigate (which will fail immediately if an
    alert is present).

--- lib/capybara/accessible/adapters/selenium.rb
+++ lib/capybara/accessible/adapters/selenium.rb
@@ -3,10 +3,10 @@ module Capybara::Accessible
     class Selenium
       def modal_dialog_present?(driver)
         begin
-          driver.browser.switch_to.alert
-          true
-        rescue ::Selenium::WebDriver::Error::NoAlertOpenError, ::NoMethodError
+          driver.browser.navigate.to('about:blank')
           false
+        rescue ::Selenium::WebDriver::Error::UnhandledAlertError
+          true
         end
       end
 
